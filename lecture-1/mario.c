54bf8c1eceb6f34bfe2acf7556710c5551cbf746
zwelhtetyan <zwelhtetyan2001@gmail.com>
2023-01-05 19:01:54 +0630

    init

---
+++ lecture-1/mario.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int get_size(void);
+void print_grid(int size);
+
+int main(void) {
+  // get size of grid
+  int size = get_size();
+
+  // print grid of bricks
+  print_grid(size);
+}
+
+int get_size(void) {
+  int size;
+
+  do {
+    printf("Size: ");
+    scanf("%d", &size);
+  } while (size < 1);
+
+  return size;
+}
+
+void print_grid(int size) {
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size; j++) printf("#");
+
+    printf("\n");
+  }
+}
