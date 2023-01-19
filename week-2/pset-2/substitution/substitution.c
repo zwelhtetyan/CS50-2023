7f48df0471021fc7d51e1de71bdb30f0b33cf599
zwelhtetyan <zwelhtetyan2001@gmail.com>
2023-01-19 11:45:50 +0630

    fixed key validation

--- week-2/pset-2/substitution/substitution.c
+++ week-2/pset-2/substitution/substitution.c
@@ -5,19 +5,38 @@
 
 int main(int argc, string argv[])
 {
+  string key = argv[1];
+  int key_length = strlen(key);
+
   if (argc != 2)
   {
     printf("Usage: ./substitution key\n");
     return 1;
   }
 
-  string key = argv[1];
-  int key_length = strlen(key);
-
   if (key_length != 26)
   {
     printf("Key must contain 26 characters.\n");
     return 1;
+  }
+
+  for (int i = 0; i < key_length; i++)
+  {
+    if (isdigit(key[i]))
+    {
+      printf("Key must only contain alphabetic characters.\n");
+      return 1;
+    }
+    else {
+      for (int j = 0; j < key_length; j++)
+      {
+        if (tolower(key[j]) == tolower(key[i]))
+        {
+          printf("Key must not contain repeated characters.\n");
+          return 1;
+        }
+      }
+    }
   }
 
   const string plaintext = get_string("plaintext: ");
@@ -26,13 +45,13 @@ int main(int argc, string argv[])
 
   for (int i = 0, N = strlen(plaintext); i < N; i++)
   {
-    char letter = tolower(plaintext[i]);
+    char letter = plaintext[i];
 
     if (letter >= 'A' && letter <= 'Z')
     {
-      printf("%c", key[letter - 'A']);
+      printf("%c", toupper(key[letter - 'A']));
     }
-    if (letter >= 'a' && letter <= 'z')
+    else if (letter >= 'a' && letter <= 'z')
     {
       printf("%c", tolower(key[letter - 'a']));
     }
