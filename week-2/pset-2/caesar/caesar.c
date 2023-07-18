911c6ad7ebdb6ae288af700f0bce4a8b331a6ba7
zwelhtetyan <zwelhtetyan2001@gmail.com>
2023-07-18 14:40:48 +0630

    chore(week-2): update caesar

--- week-2/pset-2/caesar/caesar.c
+++ week-2/pset-2/caesar/caesar.c
@@ -6,30 +6,30 @@
 
 int main(int argc, string argv[])
 {
-    if (argc != 2)   //to check if command line argument is not equal to 2
+    if (argc != 2)
     {
         printf("Usage: ./caesar key\n");
         return 1;
     }
 
-    int arg_length = strlen(argv[1]);    //to get the length of string in command line argument
-    for (int i = 0; i < arg_length; i++)  //iterate through the length of the argv[1]
+    int arg_length = strlen(argv[1]);    // to get the length of string in command line argument
+    for (int i = 0; i < arg_length; i++) // iterate through the length of the argv[1]
     {
-        if (!isdigit(argv[1][i]))  //to check if character  in argv[1] is not digit
+        if (!isdigit(argv[1][i])) // to check if character  in argv[1] is not digit
         {
-            printf("Usage: ./caesar key");  //if not print this //
+            printf("Usage: ./caesar key");
             return 1;
         }
     }
 
-    int key = atoi(argv[1]); //convert string to integer
+    int key = atoi(argv[1]); // convert string to integer
 
-    string plaintext = get_string("plaintext: ");  //get plaintext to encipher  from user
+    string plaintext = get_string("plaintext: "); // get plaintext from user
 
-    printf("ciphertext: "); //print the encipher text here
+    printf("ciphertext: "); // print the encipher text here
 
-    int plaintext_length = strlen(plaintext);  //to check the length of plaintext
-    for (int i = 0; i < plaintext_length; i++)  //iterate through the length of plaintext
+    int plaintext_length = strlen(plaintext);  // to check the length of plaintext
+    for (int i = 0; i < plaintext_length; i++) // iterate through the length of plaintext
     {
         if (isupper(plaintext[i]))
         {
