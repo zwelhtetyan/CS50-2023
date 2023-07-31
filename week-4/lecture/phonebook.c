616f4fd1a040bd859c5d92493fd43df0d4bd4631
zwelhtetyan <zwelhtetyan2001@gmail.com>
2023-07-31 13:34:28 +0630

    w-4(lecture): phonebook

---
+++ week-4/lecture/phonebook.c
@@ -0,0 +1,14 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+  FILE *file = fopen("phonebook.csv", "a");
+
+  string name = get_string("Name: ");
+  string number = get_string("Number: ");
+
+  fprintf(file, "%s, %s\n", name, number);
+
+  fclose(file);
+}
