40a69ed444eacad92481d5afda23fa3368036a06
zwelhtetyan <zwelhtetyan2001@gmail.com>
2023-08-10 00:05:23 +0630

    w-4(pset): implement grayscale

--- week-4/pset-4/filter-less/helpers.c
+++ week-4/pset-4/filter-less/helpers.c
@@ -1,8 +1,24 @@
 #include "helpers.h"
+#include "math.h"
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int red = image[i][j].rgbtRed;
+            int green = image[i][j].rgbtGreen;
+            int blue = image[i][j].rgbtBlue;
+
+            int avg = round((red + green + blue) / 3);
+
+            image[i][j].rgbtRed = avg;
+            image[i][j].rgbtGreen = avg;
+            image[i][j].rgbtBlue = avg;
+        }
+    }
     return;
 }
 
