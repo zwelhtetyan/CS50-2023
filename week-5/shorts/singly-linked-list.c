e33656493ca2793cc71095d24586e9290e83034e
zwelhtetyan <zwelhtetyan2001@gmail.com>
2023-09-29 15:26:25 +0630

    w-5(single-linked-list): insert new node

--- week-5/shorts/singly-linked-list.c
+++ week-5/shorts/singly-linked-list.c
@@ -16,9 +16,11 @@ node *root = NULL;
 node *create(int number);
 int init_nodes(int argc, char *argv[]);
 bool find(node *find_node, int number);
+node *insert(node *new_node, int value);
 
 int main(int argc, char *argv[])
 {
+    // base case
     if (argc < 2)
     {
         printf("Usage: ./singly-lined-list 2 3 4 etc...\n");
@@ -31,13 +33,22 @@ int main(int argc, char *argv[])
     // find numbers
     bool exists = find(root, 3);
 
-    printf("%s\n", exists ? "true" : "false");
-
-    while (root != NULL)
+    // insert new node
+    node *new_node = malloc(sizeof(node));
+    if (new_node == NULL)
     {
-        printf("%i\n", root->number);
-        root = root->next;
+        return 1;
     }
+
+    insert(new_node, 11);
+
+    // printf("%s\n", exists ? "true" : "false");
+
+    // while (root != NULL)
+    // {
+    //     printf("%i\n", root->number);
+    //     root = root->next;
+    // }
 
     return 0;
 }
@@ -60,6 +71,18 @@ node *create(int number)
     return temp;
 }
 
+node *insert(node *new_node, int value)
+{
+    new_node->number = value;
+    new_node->next = NULL;
+
+    new_node->next = root;
+
+    root = new_node;
+
+    return new_node;
+}
+
 bool find(node *find_node, int number)
 {
     if (find_node == NULL)
@@ -74,6 +97,7 @@ bool find(node *find_node, int number)
     else
     {
         find(find_node->next, number); // recursion here
+        return false;
     }
 }
 
