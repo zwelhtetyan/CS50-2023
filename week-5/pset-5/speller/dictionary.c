c1c7a41d3cb6784d8f4c22ca5edad5bce7f7d780
zwelhtetyan <zwelhtetyan2001@gmail.com>
2023-10-06 16:12:12 +0630

    w-5(lab): speller init

---
+++ week-5/pset-5/speller/dictionary.c
@@ -0,0 +1,67 @@
+// Implements a dictionary's functionality
+
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+#include "dictionary.h"
+
+// Represents a node in a hash table
+typedef struct node
+{
+    char word[LENGTH + 1];
+    struct node *next;
+} node;
+
+// TODO: Choose number of buckets in hash table
+const unsigned int N = 26;
+
+// Hash table
+node *table[N];
+
+// Returns true if word is in dictionary, else false
+bool check(const char *word)
+{
+    // TODO
+    return false;
+}
+
+// Hashes word to a number
+unsigned int hash(const char *word)
+{
+    // TODO: Improve this hash function
+    return toupper(word[0]) - 'A';
+}
+
+// Loads dictionary into memory, returning true if successful, else false
+bool load(const char *dictionary)
+{
+    FILE *infile = fopen(dictionary, "r");
+    if (!infile)
+    {
+        return false;
+    }
+
+    char word[LENGTH + 1];
+    while (fscanf(infile, "%s", word) == 1)
+    {
+        printf("%s\n", word);
+    }
+
+    // TODO
+    return false;
+}
+
+// Returns number of words in dictionary if loaded, else 0 if not yet loaded
+unsigned int size(void)
+{
+    // TODO
+    return 0;
+}
+
+// Unloads dictionary from memory, returning true if successful, else false
+bool unload(void)
+{
+    // TODO
+    return false;
+}
