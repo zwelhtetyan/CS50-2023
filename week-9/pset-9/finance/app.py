de0a4c51019c08ab324c5271af9df956a409f667
zwelhtetyan <zwelhtetyan2001@gmail.com>
2023-10-19 23:12:11 +0630

    w-9(pset/finance): implementing register

--- week-9/pset-9/finance/app.py
+++ week-9/pset-9/finance/app.py
@@ -61,21 +61,23 @@ def login():
 
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
+        # Get form data
+        username = request.form.get("username").strip()
+        password = request.form.get("password").strip()
 
         # Ensure username was submitted
-        if not request.form.get("username"):
+        if not username:
             return apology("must provide username", 403)
 
         # Ensure password was submitted
-        elif not request.form.get("password"):
+        elif not password:
             return apology("must provide password", 403)
 
         # Query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = ?",
-                          request.form.get("username"))
+        rows = db.execute("SELECT * FROM users WHERE username = ?", username)
 
         # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], password):
             return apology("invalid username and/or password", 403)
 
         # Remember which user has logged in
@@ -110,7 +112,55 @@ def quote():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
-    return apology("TODO")
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        # Get form data
+        username = request.form.get("username").strip()
+        password = request.form.get("password").strip()
+        confirmation = request.form.get("confirmation").strip()
+
+        # Ensure username was submitted
+        if not username:
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not password or not confirmation:
+            return apology("must provide both password and confirmation", 403)
+
+        elif password != confirmation:
+            return apology("passwords do not match", 401)
+
+        # Query database for username
+        rows_username = db.execute(
+            "SELECT username FROM users WHERE username = ?", username)
+
+        # check user is already exists
+        if len(rows_username):
+            return apology("username already exists", 409)
+
+        # generate hashed password
+        hash = generate_password_hash(password)
+
+        # add user into db
+        db.execute(
+            "INSERT INTO users (username, hash) VALUES(?, ?)", username, hash)
+
+        # get his/her id
+        rows_id = db.execute(
+            "SELECT id FROM users WHERE username = ?", username)
+
+        session["user_id"] = rows_id[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("register.html")
 
 
 @app.route("/sell", methods=["GET", "POST"])
